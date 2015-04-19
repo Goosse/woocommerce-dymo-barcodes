bc09e734af15b6e6dfbb09c0b90f424a4dca5124
Jack Gregory <jackjlgregory@gmail.com>
2015-04-19 19:30:16 +0100

    refactor product barcodes table

--- includes/class-wc-product-barcodes-table.php
+++ includes/class-wc-product-barcodes-table.php
@@ -42,7 +42,6 @@ class WC_Product_Barcodes_Table extends WP_List_Table {
 	 * Don't need this
 	 */
 	public function display_tablenav( $position ) {
-
 		if ( $position != 'top' ) {
 			parent::display_tablenav( $position );
 		}
@@ -52,7 +51,6 @@ class WC_Product_Barcodes_Table extends WP_List_Table {
 	 * Output the report
 	 */
 	public function output_report() {
-
 		$this->prepare_items();
 		$this->display();
 	}
@@ -63,24 +61,15 @@ class WC_Product_Barcodes_Table extends WP_List_Table {
 	 * @param mixed $item
 	 * @param mixed $column_name
 	 */
-	public function column_default( $item, $column_name ) {
-		global $product;
-
-		if ( ! $product || $product->id !== $item->id ) {
-			$product = wc_get_product( $item->id );
-		}
+	public function column_default( $product, $column_name ) {
+		$action_id = $product->is_type( 'variation' ) ? $product->parent->id : $product->id;
 		
-		$action_id = $product->is_type( 'variation' ) ? $item->parent : $item->id;
-
 		switch( $column_name ) {
-
 			case 'product_image' :	
-      echo '<a href="' . get_edit_post_link( $action_id ) . '">' . $product->get_image() . '</a>';
+      			echo '<a href="' . get_edit_post_link( $action_id ) . '">' . $product->get_image() . '</a>';
 			break;
 			
 			case 'product' :
-				echo '<a href="' . get_edit_post_link( $action_id ) . '">' . $product->get_title() . '</a>';
-
 				// Get variation data
 				if ( $product->is_type( 'variation' ) ) {
 					$list_attributes = array();
@@ -90,20 +79,22 @@ class WC_Product_Barcodes_Table extends WP_List_Table {
 						$list_attributes[] = ucwords( str_replace( '-', ' ', $attribute ) );
 					}
 
+					echo '<a href="' . get_edit_post_link( $action_id ) . '">' . $product->parent->get_title() . '</a>';
 					echo '<br>' . implode( ' / ', $list_attributes );
+				} else {
+					echo '<a href="' . get_edit_post_link( $action_id ) . '">' . $product->get_title() . '</a>';
 				}
 			break;
-
 			case 'product_price' :
 				echo $product->get_price_html() ? $product->get_price_html() : '<span class="na">&ndash;</span>';
 			break;
 			
 			case 'product_sku' :
 				if ( $sku = $product->get_sku() ) {
-				   echo $sku;
+					echo $sku;
 				} else {
-  				echo '<span class="na">&ndash;</span>';
-        }
+  					echo '<span class="na">&ndash;</span>';
+        		}
 			break;
 
 			case 'stock_level' :
@@ -111,41 +102,38 @@ class WC_Product_Barcodes_Table extends WP_List_Table {
 			break;
 
 			case 'wcb_barcodes' :
-				?><p>
-					<?php
+				echo '<p>';
+  				echo "<input type='number' class='product-label-input' value='0' min='0' tabindex='1'>";
   					
-  					echo "<input type='number' class='product-label-input' value='0' min='0' tabindex='1'>";
+  				$options = get_option( 'woocommerce_product_barcodes_settings' );
   					
-  					$options = get_option( 'woocommerce_product_barcodes_settings' );
-  					
-  					if ( $sku = $product->get_sku() ) {
-              $barcode = $sku;
-				    } else {
+  				if ( $sku = $product->get_sku() ) {
+              		$barcode = $sku;
+				} else {
   				    $barcode = $product->id;
-				    }
+				}
 				    
-				    $metadata = array();
+				$metadata = array();
 				    
-				    $metadata[] = $options['show_price'] == 'yes' ? get_woocommerce_currency_symbol() . wc_format_decimal( $product->get_price(), 2 ) : '';
-            $metadata[] = $options['show_sku'] == 'yes' ? $barcode : '';	
+				$metadata[] = $options['show_price'] == 'yes' ? get_woocommerce_currency_symbol() . wc_format_decimal( $product->get_price(), 2 ) : '';
+            	$metadata[] = $options['show_sku'] == 'yes' ? $barcode : '';	
 				    
-				    // Get variation data
-            if ( $product->is_type( 'variation' ) ) {
-              $list_attributes = array();
-              $attributes = $product->get_variation_attributes();
+				// Get variation data
+	            if ( $product->is_type( 'variation' ) ) {
+		            	$list_attributes = array();
+		            	$attributes = $product->get_variation_attributes();
 
-              foreach ( $attributes as $name => $attribute ) {
-                $list_attributes[] = ucwords( str_replace( '-', ' ', $attribute ) );
-					    }
-					    
-              $metadata[] = $options['show_option'] == 'yes' ? join(' / ', $list_attributes ) : '';
-              echo "<input type='hidden' class='product-metadata' value='".esc_attr( join( ' ', $metadata ) )."'>";
-					  }
+		              	foreach ( $attributes as $name => $attribute ) {
+		               		$list_attributes[] = ucwords( str_replace( '-', ' ', $attribute ) );
+						}
+							    
+		              	$metadata[] = $options['show_option'] == 'yes' ? join(' / ', $list_attributes ) : '';
+	              		echo "<input type='hidden' class='product-metadata' value='" . esc_attr( join( ' ', $metadata ) ) . "'>";
+					}
             
-						echo "<input type='hidden' class='product-name' value='".esc_attr( $product->get_title() )."'>";
-            echo "<input type='hidden' class='product-barcode' value='".esc_attr( $barcode )."'>";
-					?>
-				</p><?php
+				echo "<input type='hidden' class='product-name' value='" . esc_attr( $product->get_title() ) . "'>";
+            	echo "<input type='hidden' class='product-barcode' value='" . esc_attr( $barcode ) . "'>";
+				echo '</p>';
 			break;
 		}
 	}
@@ -154,72 +142,71 @@ class WC_Product_Barcodes_Table extends WP_List_Table {
 	 * get_columns function.
 	 */
 	public function get_columns() {
-
 		$columns = array(
-  		'product_image'      => '<span class="wc-image tips" data-tip="' . __( 'Image', 'woocommerce' ) . '">' . __( 'Image', 'woocommerce' ) . '</span>',
-			'product'            => __( 'Product', 'wc-product-barcodes' ),
-			'product_price'      => __( 'Price', 'wc-product-barcodes' ),
-      'product_sku'        => __( 'Product SKU', 'wc-product-barcodes' ),
-			'stock_level'        => __( 'Quantity', 'wc-product-barcodes' ),
-			'wcb_barcodes'       => __( 'Barcodes', 'wc-product-barcodes' ),
+  			'product_image'     => '<span class="wc-image tips" data-tip="' . __( 'Image', 'woocommerce' ) . '">' . __( 'Image', 'woocommerce' ) . '</span>',
+			'product'           => __( 'Product', 'wc-product-barcodes' ),
+			'product_price'     => __( 'Price', 'wc-product-barcodes' ),
+      		'product_sku'       => __( 'SKU', 'wc-product-barcodes' ),
+			'stock_level'       => __( 'Quantity', 'wc-product-barcodes' ),
+			'wcb_barcodes'      => __( 'Barcodes', 'wc-product-barcodes' ),
 		);
 
 		return $columns;
 	}
 
-	/**
-	 * prepare_items function.
-	 */
-	public function prepare_items() {
-
-		$this->_column_headers = array( $this->get_columns(), array(), $this->get_sortable_columns() );
-		$current_page          = absint( $this->get_pagenum() );
-		$per_page              = apply_filters( 'woocommerce_product_barcodes_products_per_page', 20 );
-
-		$this->get_items( $current_page, $per_page );
-
-		/**
-		 * Pagination
-		 */
-		$this->set_pagination_args( array(
-			'total_items' => $this->max_items,
-			'per_page'    => $per_page,
-			'total_pages' => ceil( $this->max_items / $per_page )
-		) );
-	}
-	
-  /**
+  	/**
 	 * Get products post type.
 	 *
 	 * @access public
 	 * @return array
 	 */
-	public function get_items( $current_page, $per_page ) {
-		global $wpdb;
+	public function prepare_items() {
 		
-		$this->max_items = 0;
-		$this->items     = array();
-		
-		$query_from = "FROM {$wpdb->posts} as posts
-			INNER JOIN {$wpdb->postmeta} AS postmeta ON posts.ID = postmeta.post_id
-			INNER JOIN {$wpdb->postmeta} AS postmeta2 ON posts.ID = postmeta2.post_id
-			WHERE 1=1
-			AND posts.post_type IN ( 'product', 'product_variation' )
-			AND postmeta2.meta_key = '_manage_stock' AND postmeta2.meta_value = 'yes'
-			";
-		
+		$per_page = apply_filters( 'woocommerce_product_barcodes_products_per_page', 20 );
+		$columns  = $this->get_columns();
+		$hidden   = array();
+		$sortable = $this->get_sortable_columns();
+
+		// Column headers
+		$this->_column_headers = array( $columns, $hidden, $sortable );
+
+		$current_page = $this->get_pagenum();
+
+		// Query args
+		$args = array(
+			'post_type'           => array( 
+				'product', 
+				'product_variation' 
+			),
+			'posts_per_page'      => $per_page,
+			'ignore_sticky_posts' => true,
+			'paged'               => $current_page
+		);
+
 		if ( isset( $_REQUEST['id'] ) ) {
-  		
-  		$product = wc_get_product( $_REQUEST['id'] );
-  				
-  		if( $product->has_child() ) {
-    		$query_from .= " AND posts.post_parent = '".$_REQUEST['id']."' " ;
-  		} else {
-    		$query_from .= " AND posts.ID = '".$_REQUEST['id']."' ";
-      }
+	  		$product = wc_get_product( $_REQUEST['id'] );
+	  				
+	  		if( $product->has_child() ) {
+	    		$args['post_parent__in'] = array( $_REQUEST['id'] );
+	  		} else {
+	    		$args['post__in'] = array( $_REQUEST['id'] );
+	      	}
 		}
 
-		$this->items     = $wpdb->get_results( $wpdb->prepare( "SELECT posts.ID as id, posts.post_parent as parent {$query_from} GROUP BY posts.ID ORDER BY posts.post_title DESC LIMIT %d, %d;", ( $current_page - 1 ) * $per_page, $per_page ) );
-		$this->max_items = $wpdb->get_var( "SELECT COUNT( DISTINCT posts.ID ) {$query_from};" );
+		$products = new WP_Query( $args );
+
+		$items = array();
+
+		foreach( $products->posts as $item ) {
+			$product = wc_get_product( $item->ID );
+			
+			if( $product->is_type( 'variable' ) && $product->has_child() ) {
+				continue;
+			}
+
+			$items[] = $product;
+		}
+
+		$this->items = $items;
 	}
 }
