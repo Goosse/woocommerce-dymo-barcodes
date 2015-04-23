3c5e56dfdfccd5cb315819c0787c3a27531939e5
Jack Gregory <jackjlgregory@gmail.com>
2015-04-23 17:44:10 +0100

    js coding standards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,139 +1,139 @@
-(function($) {
-	var label, labelSet, labelSize,
-	    stock = 1;
+( function( $ ) {
+    var label, labelSet, labelSize,
+        stock = 1;
 
-	function init() {
-		loadLabel();
-		loadPrinters();
-	}
+    function init() {
+        loadLabel();
+        loadPrinters();
+    }
 
-	function loadLabel() {
-		var labelSize = wcb_params.label_size,
-			  $printerList = $('#woocommerce_product_barcodes_label_size');
+    function loadLabel() {
+        var labelSize = wcb_params.label_size,
+            $printerList = $( '#woocommerce_product_barcodes_label_size' );
 
-		if ($printerList.length) {
-		  labelSize = $printerList.val();
-		}
+        if ( $printerList.length ) {
+            labelSize = $printerList.val();
+        }
 
-		getLabel(labelSize);
-	}
+        getLabel( labelSize );
+    }
 
-	function loadPrinters() {
-		var $printerList = $("#woocommerce_product_barcodes_dymo_printer"),
-			  printers = dymo.label.framework.getLabelWriterPrinters();
+    function loadPrinters() {
+        var $printerList = $( "#woocommerce_product_barcodes_dymo_printer" ),
+            printers = dymo.label.framework.getLabelWriterPrinters();
 
-		for (var i = 0; i < printers.length; ++i) {
-			var printer = printers[i],
-				  printerName = printer.name,
-      
-      option = $('<option>').val(printerName).text(printerName);
-			$printerList.append(option);
-		}
-	}
-	
-	function getLabel(size) {
-		$.get(wcb_params.plugin_url + "/assets/labels/" + size + ".label", function(labelXml) {
-			label = dymo.label.framework.openLabelXml(labelXml);
-			renderLabel();
-			printPreview();
-		}, "text");
-	}
+        for ( var i = 0; i < printers.length; ++i ) {
+            var printer = printers[ i ],
+                printerName = printer.name,
 
-	function printLabel(data, event) {
-		try {
-			if (!label) {
-  			throw wcb_params.label_loaded_error;
-  		}
-			if (!labelSet) {
-  			throw wcb_params.data_loaded_error;
-  		}
-			label.print(wcb_params.dymo_printer, null, data);
-		} catch (event) {
-			alert(event.message || event);
-		}
-	}
+                option = $( '<option>' ).val( printerName ).text( printerName );
+            $printerList.append( option );
+        }
+    }
 
-	function createLabelSet() {
-		var labelSet = new dymo.label.framework.LabelSetBuilder();
+    function getLabel( size ) {
+        $.get( wcb_params.plugin_url + "/assets/labels/" + size + ".label", function( labelXml ) {
+            label = dymo.label.framework.openLabelXml( labelXml );
+            renderLabel();
+            printPreview();
+        }, "text" );
+    }
 
-		$('.wcb_barcodes').each(function(index, value) {
-			var $variation = $(this),
-				  name = $variation.find('input.product-name').val(),
-          barcode = $variation.find('input.product-barcode').val(),
-          metadata = $variation.find('input.product-metadata').val(),
-          stock = $variation.find('input.product-label-input').val();
+    function printLabel( data, event ) {
+        try {
+            if ( !label ) {
+                throw wcb_params.label_loaded_error;
+            }
+            if ( !labelSet ) {
+                throw wcb_params.data_loaded_error;
+            }
+            label.print( wcb_params.dymo_printer, null, data );
+        } catch ( event ) {
+            alert( event.message || event );
+        }
+    }
 
-			for (var i = 0; i < stock; i++) {
-				console.log(name + ' : ' + metadata + ' : ' + barcode);
-				var record = labelSet.addRecord();
-				record.setText("product_name", name);
-				record.setText("metadata", $.trim(metadata));
-				record.setText("product_barcode", barcode);
-			}
-		});
+    function createLabelSet() {
+        var labelSet = new dymo.label.framework.LabelSetBuilder();
 
-		return labelSet;
-	}
+        $( '.wcb_barcodes' ).each( function( index, value ) {
+            var $variation = $( this ),
+                name = $variation.find( 'input.product-name' ).val(),
+                barcode = $variation.find( 'input.product-barcode' ).val(),
+                metadata = $variation.find( 'input.product-metadata' ).val(),
+                stock = $variation.find( 'input.product-label-input' ).val();
 
-	function printPreview() {
-		var $preview = $('#woocommerce-dymo-print-preview-img'),
-			pngData = label.render();
-		$preview.attr('src', 'data:image/png;base64,' + pngData);
-	}
+            for ( var i = 0; i < stock; i++ ) {
+                var record = labelSet.addRecord();
+                record.setText( "product_name", name );
+                record.setText( "metadata", $.trim( metadata ) );
+                record.setText( "product_barcode", barcode );
+            }
+        } );
 
-	function renderLabel() {
-		var metadata = $('.metadata:checked').map(function() {
-			return $.trim($(this).parent().text());
-		}).get().join(' ');
+        return labelSet;
+    }
 
-		var name = $('.name:checked').map(function() {
-			return $.trim($(this).parent().text());
-		}).get().join(' ');
-		
-		var barcode = $('.barcode:checked').map(function() {
-			return $.trim('1234');
-		}).get().join(' ');
+    function printPreview() {
+        var $preview = $( '#woocommerce-dymo-print-preview-img' ),
+            pngData = label.render();
+        $preview.attr( 'src', 'data:image/png;base64,' + pngData );
+    }
 
-		label.setObjectText('metadata', metadata);
-		label.setObjectText('product_name', name);
-		label.setObjectText('product_barcode', barcode);
-	}
+    function renderLabel() {
+        var metadata = $( '.metadata:checked' ).map( function() {
+            return $.trim( $( this ).parent().text() );
+        } ).get().join( ' ' );
 
-	// load label and defaults
-	$(window).on('load', init);
+        var name = $( '.name:checked' ).map( function() {
+            return $.trim( $( this ).parent().text() );
+        } ).get().join( ' ' );
 
-	$(document).on('change', '#woocommerce_product_barcodes_label_size', function() {
-		var labelSize = $(this).val();
-		getLabel(labelSize);
-	});
+        var barcode = $( '.barcode:checked' ).map( function() {
+            return $.trim( '1234' );
+        } ).get().join( ' ' );
 
-	$(document).on('change', '.label-preview-option', function() {
-		renderLabel();
-		printPreview();
-	});
+        label.setObjectText( 'metadata', metadata );
+        label.setObjectText( 'product_name', name );
+        label.setObjectText( 'product_barcode', barcode );
+    }
 
-	// update print button
-	$(document).on('change keyup', '.product-label-input', function() {
-		var sum = 0,
-			  $printBnt = $('#wcb_print_btn');
-			  
-		$('.product-label-input').each(function() {
-			sum += Number($(this).val());
-			if (wcb_params.dymo_printer) {
-				if (sum > 0) {
-					$printBnt.prop('disabled', false).find('span').text(sum);
-				} else {
-					$printBnt.prop('disabled', true).find('span').text('');
-				}
-			}
-		});
-	});
+    // load label and defaults
+    $( window ).on( 'load', init );
 
-	$(document).on('click', '#wcb_print_btn', function(event) {
-		event.preventDefault();
-		labelSet = createLabelSet();
-		printLabel(labelSet, event);
-		return false;
-	});
+    $( document ).on( 'change', '#woocommerce_product_barcodes_label_size', function() {
+        var labelSize = $( this ).val();
+        getLabel( labelSize );
+    } );
 
-})(jQuery);
+    $( document ).on( 'change', '.label-preview-option', function() {
+        renderLabel();
+        printPreview();
+    } );
+
+    // update print button
+    $( document ).on( 'change keyup', '.product-label-input', function() {
+        var sum = 0,
+            $printButton = $( '#wcb_print_btn' );
+
+        $( '.product-label-input' ).each( function() {
+            sum += Number( $( this ).val() );
+            if ( wcb_params.dymo_printer ) {
+                if ( sum > 0 ) {
+                    $printButton.prop( 'disabled', false ).find( 'span' ).text( sum );
+                } else {
+                    $printButton.prop( 'disabled', true ).find( 'span' ).text( '' );
+                }
+            }
+        } );
+    } );
+
+    // print
+    $( document ).on( 'click', '#wcb_print_btn', function( e ) {
+        event.preventDefault();
+        labelSet = createLabelSet();
+        printLabel( labelSet, e );
+        return false;
+    } );
+
+} )( jQuery );
